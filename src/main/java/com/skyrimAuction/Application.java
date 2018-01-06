0687d3b3f0fb6b90bd6fe6122044af2cd291bd8b
= <=>
2018-01-06 17:34:13 +0300

    pom.xml modified, dependencies added. Added carcass of telegram bot.

--- src/main/java/com/skyrimAuction/Application.java
+++ src/main/java/com/skyrimAuction/Application.java
@@ -1,9 +1,16 @@
 package com.skyrimAuction;
 
+import com.skyrimAuction.telegramBot.SkyrimAuctionTelegramBot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
+import org.telegram.telegrambots.generics.BotOptions;
+import org.telegram.telegrambots.generics.BotSession;
+import org.telegram.telegrambots.generics.LongPollingBot;
 
 @SpringBootApplication
 public class Application {
@@ -13,7 +20,19 @@ public class Application {
         return new BCryptPasswordEncoder();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws TelegramApiRequestException {
         SpringApplication.run(Application.class, args);
+        ApiContextInitializer.init();
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+        BotSession skyrimAuctionSession = null;
+
+            skyrimAuctionSession = botsApi.registerBot(new SkyrimAuctionTelegramBot());
+
+       while (true) {
+            if (!skyrimAuctionSession.isRunning()) {
+                skyrimAuctionSession.stop();
+                skyrimAuctionSession.start();
+            }
+       }
     }
 }
---
+++ src/main/java/com/skyrimAuction/telegramBot/SkyrimAuctionTelegramBot.java
@@ -0,0 +1,44 @@
+package com.skyrimAuction.telegramBot;
+
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+import java.io.InvalidObjectException;
+
+public class SkyrimAuctionTelegramBot extends TelegramLongPollingBot {
+
+    public void sendMessage(Message message, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setText(text);
+        try {
+            sendMessage(sendMessage);
+
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void switchCommand(Message message, String textMessage) throws InvalidObjectException {
+
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+}
