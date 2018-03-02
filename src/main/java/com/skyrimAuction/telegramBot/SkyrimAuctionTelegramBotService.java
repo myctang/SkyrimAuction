a8368a3250d4e5d7adac6871cff6d6bd77e5fea5
M1LKYWVY <ruslan.filichkin@icloud.com>
2018-03-03 00:32:53 +0300

    Small code refactoring

--- src/main/java/com/skyrimAuction/telegramBot/SkyrimAuctionTelegramBotService.java
+++ src/main/java/com/skyrimAuction/telegramBot/SkyrimAuctionTelegramBotService.java
@@ -35,8 +35,4 @@ public class SkyrimAuctionTelegramBotService {
         return sellingItemService;
     }
 
-    public static QuestService getQuestService() {
-        return questService;
-    }
-
 }
