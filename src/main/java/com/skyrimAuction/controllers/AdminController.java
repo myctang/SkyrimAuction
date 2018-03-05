e14ac1de7e8eaa0cfa4285e0a2012e264ef6b178
Igor <myctang154@gmail.com>
2018-03-05 12:55:37 +0300

    Added admin routes

--- src/main/java/com/skyrimAuction/controllers/AdminController.java
+++ src/main/java/com/skyrimAuction/controllers/AdminController.java
@@ -1,5 +1,6 @@
 package com.skyrimAuction.controllers;
 
+import com.skyrimAuction.dataBaseService.entities.InventoryItem;
 import com.skyrimAuction.dataBaseService.entities.Item;
 import com.skyrimAuction.dataBaseService.entities.User;
 import com.skyrimAuction.dataBaseService.services.ItemService;
@@ -8,11 +9,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("admin")
 public class AdminController {
     @Autowired
     UserService userService;
@@ -20,20 +25,30 @@ public class AdminController {
     @Autowired
     ItemService itemService;
 
-    @GetMapping("admin/check")
+    @GetMapping("/check")
     public ResponseEntity<String> checkAdmin(){
         return new ResponseEntity<>("{\"status\":\"done\"}", HttpStatus.ACCEPTED);
     }
 
-    @GetMapping("admin/users")
+    @GetMapping("/users")
     public List<User> getUsersList(){
         return userService.getUsers();
     }
 
-    @GetMapping("admin/items")
+    @GetMapping("/items")
     public List<Item> getItemsList(){
         return itemService.getItems();
     }
 
+    @PostMapping("/setItem")
+    public ResponseEntity<Boolean> setItemToUser(int userId, int itemId){
+        User userFromBase = userService.getUser(userId);
+        Item itemFromBase = itemService.getItem(itemId);
+        if (userFromBase == null || itemFromBase == null){
+            return new ResponseEntity<>(false, HttpStatus.BAD_REQUEST);
+        }
+        userService.addItem(itemFromBase, userFromBase);
+        return new ResponseEntity<>(true, HttpStatus.OK);
+    }
 
 }
